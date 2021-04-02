cfcce19727c2b4f888b833852f3ea3d256e99aad
enzzo-dev <71385963+enzzo-dev@users.noreply.github.com>
2021-04-02 07:25:19 -0300

    Corrigir erros na build do projeto pelo visual studio 2019

---
+++ Senai_Peoples.WebApi/Domains/funcionariosDomain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Senai_Peoples.WebApi.Interfaces;
+
+namespace Senai_Peoples.WebApi.Domains
+{
+    public class funcionariosDomain
+    {
+        public int idUser { get; set; }
+        public string nome { get; set; }
+        public string sobrenome { get; set; }
+    }
+}
---
+++ Senai_Peoples.WebApi/Interfaces/IFuncionariosRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Senai_Peoples.WebApi.Domains;
+
+namespace Senai_Peoples.WebApi.Interfaces
+{
+    interface IFuncionariosRepository
+    {
+        /// <summary>
+        /// Método irá retornar todos os funcionários cadastrados no banco de dados
+        /// </summary>
+        /// <returns></returns>
+        List<funcionariosDomain> listarTodos();
+
+        /// <summary>
+        /// Método para buscar o id de algum funcionário no banco de dados
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> OBJETO </returns>
+        funcionariosDomain BuscarPorId(int id);
+
+        /// <summary>
+        /// Método irá criar um novo funcionário
+        /// </summary>
+        /// <param name="novoFuncionario"></param>
+        void Cadastrar(funcionariosDomain novoFuncionario);
+
+        /// <summary>
+        /// Método irá passar o id pelo corpo da requisição
+        /// </summary>
+        /// <param name="funcionario"></param>
+        void AtualizarIdCorpo(funcionariosDomain funcionarios);
+
+        /// <summary>
+        /// Método irá atualizar o funcionário pelo id passado pela URL
+        /// </summary>
+        /// <param name="funcionariosUrl"></param>
+        void AtualizarIdUrl(int id, funcionariosDomain funcionariosUrl);
+
+        /// <summary>
+        /// Método irá deletar usuário pelo id que foi passado no corpo da requisição ou URL
+        /// </summary>
+        /// <param name="id"></param>
+        void Deletar(int id);
+    }
+}
