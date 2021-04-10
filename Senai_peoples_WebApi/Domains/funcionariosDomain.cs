f6756ea6054f4d624311cccf9e4386652d92fa15
enzzo-dev <71385963+enzzo-dev@users.noreply.github.com>
2021-04-10 12:22:25 -0300

    Arquivos certos

---
+++ Senai_peoples_WebApi/Domains/funcionariosDomain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_peoples_WebApi.Domains
+{
+    public class funcionariosDomain
+    {
+       public int idUser { get; set; }
+       public string nome { get; set; }
+       public string sobrenome { get; set; }
+    }
+}
